bf5956c64ec12dd9676e0e70efb6eebeb2d6697f
franckalainai <franckalain.ai@gmail.com>
2022-06-28 14:30:50 +0000

    CRUD

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.products),
     path('customer/<str:pk_test>/', views.customer),
+    path('create_order/', views.createOrder, name='create-order'),
+    path('update_order/<str:pk>', views.updateOrder, name='update-order'),
+    path('delete_order/<str:pk>', views.deleteOrder, name='delete-order'),
+
 ]
