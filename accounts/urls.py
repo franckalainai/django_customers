8b9bcb7511acfb681df871b8c3b29004f60bfdda
franckalainai <franckalain.ai@gmail.com>
2022-06-28 14:46:42 +0000

    dynamic urls 2

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('products/', views.products),
-    path('customer/<str:pk_test>/', views.customer),
+    path('products/', views.products, name='products'),
+    path('customer/<str:pk_test>/', views.customer, name='customer'),
     path('create_order/', views.createOrder, name='create-order'),
     path('update_order/<str:pk>', views.updateOrder, name='update-order'),
     path('delete_order/<str:pk>', views.deleteOrder, name='delete-order'),
