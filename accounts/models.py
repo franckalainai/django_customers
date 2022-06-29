6bee401af3f7e29b9d19509855ff1ca1f633b404
franckalainai <franckalain.ai@gmail.com>
2022-06-29 16:38:08 +0000

    user role based 1

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,5 @@
-from sre_constants import CATEGORY
-from unicodedata import category
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Customer(models.Model):
